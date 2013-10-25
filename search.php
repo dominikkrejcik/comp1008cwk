a0de42f617757883e4a38ea7d4dd7ca4cdd220f2
Dom <dominik.krejcik@klikni.cz>
2013-10-25 16:22:52 +0100

    Search

---
+++ search.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<Title>Search</Title>
+<style type="text/css">
+    body { background-color: #fff; border-top: solid 10px #000;
+        color: #333; font-size: .85em; margin: 20; padding: 20;
+        font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+    }
+    h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+</style>
+</head>
+<body>
+<h1>Search</h1>
+<form method="post" action="search.php" enctype="multipart/form-data" >
+      Search  <input type="text" name="search" id="search"/></br>
+      <input type="submit" name="submit" value="Search" />
+</form>
+<?php
+    // DB connection info
+    //TODO: Update the values for $host, $user, $pwd, and $db
+    //using the values you retrieved earlier from the portal.
+    $host = "eu-cdbr-azure-west-b.cloudapp.net";
+    $user = "b028323f8b93b0";
+    $pwd = "b2732ed6";
+    $db = "domtestdb";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+    // Insert registration info
+    if(!empty($_POST)) {
+    // Retrieve data
+    $query = $_POST['search'];
+    $sql = "SELECT * FROM registration_tbl WHERE name LIKE '%$query%'";
+    $stmt = $conn->query($sql);
+    $registrants = $stmt->fetchAll(); 
+    if(count($registrants) > 0) {
+        echo "<h2>Results</h2>";
+        echo "<table>";
+        echo "<tr><th>Name</th>";
+        echo "<th>Email</th>";
+        echo "<th>Date</th>";
+        echo "<th>Company</th></tr>";
+        foreach($registrants as $registrant) {
+            echo "<tr><td>".$registrant['name']."</td>";
+            echo "<td>".$registrant['email']."</td>";
+            echo "<td>".$registrant['date']."</td>";
+            echo "<td>".$registrant['company']."</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "<h3>Nothing Fount</h3>";
+    }
+    }
+?>
+</body>
+</html>
